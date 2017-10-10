36bfdb75307a013ab3b14bdbef4cf60ff0411f95
Pedro Gabriel <pedrogabriel@protonmail.ch>
2017-10-10 23:28:59 +0100

    Add splash screen style and activity

---
+++ app/src/main/java/com/dev/moviedb/acomponents/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.dev.moviedb.acomponents.splash
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * The entrance of this app. Presents the splash screen
+ * with the app logo.
+ *
+ * @author pedrogabriel on 10/10/2017
+ * Copyright (c) 2017
+ * All rights reserved.
+ */
+class SplashActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?,
+                          persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+
+}

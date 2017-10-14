f8e0f2683c912cd3bfbec67404aab85d219a04d1
Pedro Gabriel <pedrogabriel@protonmail.ch>
2017-10-14 16:38:14 +0100

    Add CardViews with lists in the layouts

--- app/src/main/java/com/dev/moviedb/mvvm/data/source/remote/TmdbApiService.kt
+++ app/src/main/java/com/dev/moviedb/mvvm/data/source/remote/TmdbApiService.kt
@@ -17,28 +17,36 @@ interface TmdbApiService {
 
     @GET("/3/movie/popular")
     fun findMostPopularMovies(@Query("api_key") apiKey: String = RemoteCom.API_KEY_DEV,
-                              @Query("language") lang: String): Call<MovieAggregatorDTO>
+                              @Query("language") lang: String = "en"): Call<MovieAggregatorDTO>
 
     @GET("/3/movie/now_playing")
     fun findNowPlayingMovies(@Query("api_key") apiKey: String = RemoteCom.API_KEY_DEV,
-                             @Query("language") lang: String): Call<MovieAggregatorDTO>
+                             @Query("language") lang: String = "en"): Call<MovieAggregatorDTO>
 
     @GET("/3/movie/upcoming")
     fun findUpcomingMovies(@Query("api_key") apiKey: String = RemoteCom.API_KEY_DEV,
-                           @Query("language") lang: String,
+                           @Query("language") lang: String = "en",
                            @Query("page") page: Int): Call<MovieAggregatorDTO>
 
     @GET("/3/movie/{id}")
     fun findMovieById(@Path("id") id: Long,
                       @Query("api_key") apiKey: String = RemoteCom.API_KEY_DEV,
-                      @Query("language") lang: String,
+                      @Query("language") lang: String = "en",
                       @Query("append_to_response") whatToAppend: String): Call<MovieDTO>
 
     @GET("/3/search/movie")
     fun searchMoviesByKeyword(@Query("api_key") apiKey: String = RemoteCom.API_KEY_DEV,
                               @Query("query") title: String,
-                              @Query("language") lang: String,
+                              @Query("language") lang: String = "en",
                               @Query("page") page: Int): Call<MovieAggregatorDTO>
 
 
+    @GET("/3/tv/popular")
+    fun findPopularTvSeries(@Query("api_key") apiKey: String = RemoteCom.API_KEY_DEV,
+                            @Query("language") lang: String = "en")
+
+
+    @GET("/3/tv/top_rated")
+    fun findTopRatedTvSeries(@Query("api_key") apiKey: String = RemoteCom.API_KEY_DEV,
+                            @Query("language") lang: String = "en")
 }

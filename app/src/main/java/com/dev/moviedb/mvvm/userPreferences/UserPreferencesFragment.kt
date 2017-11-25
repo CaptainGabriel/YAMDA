6e118a020524941da00b2cf2d377e06d0bb3a760
PeteGabriel <pedrogabriel@protonmail.ch>
2017-11-25 16:26:58 +0000

    Add icons for options in preferences. Add preference fragment options handles.

--- app/src/main/java/com/dev/moviedb/mvvm/userPreferences/UserPreferencesFragment.kt
+++ app/src/main/java/com/dev/moviedb/mvvm/userPreferences/UserPreferencesFragment.kt
@@ -3,9 +3,13 @@ package com.dev.moviedb.mvvm.userPreferences
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import kotlinx.android.synthetic.main.fragment_user_preferences_layout.*
+import petegabriel.com.yamda.R
 
 
 /**
@@ -26,8 +30,45 @@ class UserPreferencesFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
 
+        var view = inflater?.inflate(R.layout.fragment_user_preferences_layout, container, false)
+
+        return view
+    }
+
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        simcard_preference_switch.setOnCheckedChangeListener(handleSimcardOptionChange())
+        wifi_preference_switch.setOnCheckedChangeListener(handleNetworkOptionChange())
+
+    }
+
+    /**
+     * Write the simcard option change into preferences file.
+     */
+    private fun handleSimcardOptionChange(): (CompoundButton, Boolean) -> Unit {
+        return {
+            _: CompoundButton, isChecked: Boolean ->
+            Log.d("SimcardSwitch", "Simcard checked: $isChecked")
+            if (isChecked) {
+               //TODO save it into preferences file
+            }
+        }
+    }
+
+    /**
+     * Write the network option change into preferences file.
+     */
+    private fun handleNetworkOptionChange(): (CompoundButton, Boolean) -> Unit {
+        return {
+            _: CompoundButton, isChecked: Boolean ->
+            Log.d("NetworkSwitch", "Network checked: $isChecked")
+            if (isChecked) {
+                //TODO save it into preferences file
+            }
+        }
     }
 
 }

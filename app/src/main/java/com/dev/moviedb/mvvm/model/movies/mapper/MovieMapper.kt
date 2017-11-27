4d0ad8a69bcdeeb16f16c683047bcb7a1040a42f
PeteGabriel <pedrogabriel@protonmail.ch>
2017-11-27 21:51:33 +0000

    Add mapping logic and decorator class.

--- app/src/main/java/com/dev/moviedb/mvvm/model/movies/mapper/MovieMapper.kt
+++ app/src/main/java/com/dev/moviedb/mvvm/model/movies/mapper/MovieMapper.kt
@@ -9,6 +9,10 @@ import org.mapstruct.factory.Mappers
 
 /**
  *
+ * Interface responsible for specifying the mapping logic for entities
+ * related with movies. Advanced aspects for that mapping logic should be implemented
+ * in the class used as decorator.
+ *
  * Yamda 1.0.0.
  */
 @Mapper
@@ -34,20 +38,28 @@ interface MovieMapper{
 
 
     @Mappings(
-            Mapping(source = "majorVersion", target = "major"),
-            Mapping(source = "minorVersion", target = "minor"),
-            Mapping(source = "patchVersion", target = "patch"),
-            Mapping(source = "normalVersion", target = "normal"),
-            Mapping(source = "preReleaseVersion", target = "preRelease")
+            Mapping(source = "backdropPath", target = "mMovieImages.mBackdropImagePath"),
+            Mapping(source = "id", target = "mPrimaryFacts.mId"),
+            Mapping(source = "overview", target = "mPrimaryFacts.mOverview"),
+            Mapping(source = "releaseDate", target = "mPrimaryFacts.mReleaseDate"),
+            Mapping(source = "posterPath", target = "mMovieImages.mPosterImagePath"),
+            Mapping(source = "popularity", target = "mPopularity.mPopularity"),
+            //Mapping(source = "title", target = ""),
+            //Mapping(source = "video", target = ""),
+            Mapping(source = "voteAverage", target = "mPopularity.mVoteAverage"),
+            //Mapping(source = "status", target = ""),
+            Mapping(source = "runtime", target = "mAdvancedFacts.mRuntime"),
+            Mapping(source = "tagline", target = "mAdvancedFacts.mTagLine"),
+            Mapping(source = "imdbId", target = "mAdvancedFacts.mImdbID"),
+            Mapping(source = "homepage", target = "mAdvancedFacts.mHomepage"),
+            Mapping(source = "vote_count", target = "mPopularity.mVoteCount")
+    //TODO trailers
+    //TODO genres
     )
-    fun toMovieDto(movie: Movie): MovieDTO
-
-
-    @InheritInverseConfiguration
     fun toMovie(movieDto : MovieDTO): Movie
 
 
-
-
+    @InheritInverseConfiguration
+    fun toMovieDto(movie: Movie): MovieDTO
 
 }
--- app/src/main/java/com/dev/moviedb/mvvm/model/movies/mapper/MovieMapperDecorator.kt
+++ app/src/main/java/com/dev/moviedb/mvvm/model/movies/mapper/MovieMapperDecorator.kt
@@ -1,9 +1,23 @@
 package com.dev.moviedb.mvvm.model.movies.mapper
 
+import com.dev.moviedb.mvvm.model.movies.Movie
+import com.dev.moviedb.mvvm.model.movies.dto.MovieDTO
+
 /**
+ * Decorator class that implements advanced aspects of the mapping
+ * logic between dtos and model classes.
  *
  * Yamda 1.0.0.
  */
-class MovieMapperDecorator{
+abstract class MovieMapperDecorator(val delegate : MovieMapper) : MovieMapper{
+
+
+    override fun toMovie(movieDto : MovieDTO): Movie {
+        var mov = this.delegate.toMovie(movieDto)
+
+        //TODO add custom logic here
+
+        return mov
+    }
 
 }

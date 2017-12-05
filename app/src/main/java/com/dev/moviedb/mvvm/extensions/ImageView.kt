0956b2c387a275c5d901b1c157f160ba8fb93fb0
PeteGabriel <pedrogabriel@protonmail.ch>
2017-12-05 15:05:47 +0000

    Add placeholder colors for images that cannot be loaded.

--- app/src/main/java/com/dev/moviedb/mvvm/extensions/ImageView.kt
+++ app/src/main/java/com/dev/moviedb/mvvm/extensions/ImageView.kt
@@ -3,6 +3,7 @@ package com.dev.moviedb.mvvm.extensions
 import android.widget.ImageView
 import com.dev.moviedb.mvvm.repository.remote.ApiConsts
 import com.squareup.picasso.Picasso
+import petegabriel.com.yamda.R
 
 /**
  * This file contains the various extension methods for the class @see(ImageView) to
@@ -15,19 +16,20 @@ import com.squareup.picasso.Picasso
 
 fun ImageView.loadUrl(url: String, transform: Boolean = true) {
     var loadUrl = ""
-    if (url.isEmpty()){
-        loadUrl = "https://screenshotlayer.com/images/assets/placeholder.png"
-    }else{
-        loadUrl = "${ApiConsts.IMG_BASE_URL}${ApiConsts.MEDIUM_IMG_SIZE}$url"
-    }
+    loadUrl = if (url.isEmpty()) loadUrl else "${ApiConsts.IMG_BASE_URL}${ApiConsts.MEDIUM_IMG_SIZE}$url"
+
     if (transform){
         Picasso.with(context)
                 .load(loadUrl)
+                .placeholder(R.color.material_grey_300)
+                .error(R.color.material_grey_300)
                 .transform(RoundedTransformation(7, 4))
                 .into(this)
     }else{
         Picasso.with(context)
                 .load(loadUrl)
+                .placeholder(R.color.material_grey_300)
+                .error(R.color.material_grey_300)
                 .into(this)
     }
 }

58af439f858ce8383938b692a4d1eebdad4e732b
PeteGabriel <pedrogabriel@protonmail.ch>
2018-02-20 22:16:26 +0000

    Add new activity to start implemention of flexible scrollview of details page

---
+++ app/src/main/java/com/dev/moviedb/mvvm/movieDetails/MovieDetailsFlexibleActivity.kt
@@ -0,0 +1,17 @@
+package com.dev.moviedb.mvvm.movieDetails
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+
+class MovieDetailsFlexibleActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+
+    }
+
+}
